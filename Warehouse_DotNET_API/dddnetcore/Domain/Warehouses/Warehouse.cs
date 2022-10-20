81ae28be2ecd4df517964860c71acea2d0641899
Patryk Paś <86026100+PPatricc@users.noreply.github.com>
2022-10-20 21:37:04 +0100

    Added the Warehouse dotnet API core

---
+++ Warehouse_DotNET_API/dddnetcore/Domain/Warehouses/Warehouse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DDDSample1.Domain.Shared;
+
+namespace DDDSample1.Domain.Warehouses
+{
+    public class Warehouse : Entity<WarehouseId>, IAggregateRoot
+    {
+
+        public string ID { get; private set; }
+        public string designaion { get; private set; }
+        public string address { get; private set; }
+        public double latitude { get; private set; }
+        public double longitude { get; private set; }
+
+
+        private Warehouse()
+        {
+            
+        }
+
+        public Warehouse(string id, string des, string add, double lat, double longt)
+        {
+            this.ID = id;
+            this.designaion = des;
+            this.address = add;
+            this.latitude = lat;
+            this.longitude = longt;
+        }
+
+    }
+}
---
+++ Warehouse_DotNET_API/dddnetcore/Infraestructure/Warehouses/WarehouseRepository.cs
@@ -0,0 +1,16 @@
+﻿using DDDSample1.Domain.Warehouses;
+using DDDSample1.Infrastructure.Shared;
+
+namespace DDDSample1.Infrastructure.Warehouses
+{
+    public class WarehouseRepository : BaseRepository<Warehouse, WarehouseId>, IWarehouseRepository
+    {
+
+        public WarehouseRepository(DDDSample1DbContext context) : base(context.Warehouses)
+        {
+
+        }
+
+
+    }
+}
