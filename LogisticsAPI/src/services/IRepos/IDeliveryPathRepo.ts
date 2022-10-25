9ebe0d134a760c823badadbfa5905b57fb6491a8
ben1782 <benjaminmoreira.parcoursup@gmail.com>
2022-10-25 11:56:00 +0100

    ADD_NODEJS_DELIVERY_PATH_PART

---
+++ LogisticsAPI/src/services/IRepos/IDeliveryPathRepo.ts
@@ -0,0 +1,12 @@
+import { Repo } from "../../core/infra/Repo";
+import { DeliveryPath } from "../../domain/deliverypath";
+import { DeliveryPathId } from "../../domain/deliverypathId";
+
+export default interface IDeliveryPathRepo extends Repo<DeliveryPath> {
+    save(deliverypath: DeliveryPath): Promise<DeliveryPath>;
+    findByDomainId (deliverypathId: DeliveryPathId | string): Promise<DeliveryPath>;
+    findAll (): Promise<DeliveryPath[]>;
+    //findByIds (deliverypathIds: DeliveryPathId[]): Promise<DeliveryPath[]>;
+    //saveCollection (deliverypaths: DeliveryPath[]): Promise<DeliveryPath[]>;
+    //removeByRoleIds (deliverypaths: DeliveryPathId[]): Promise<any>
+}
