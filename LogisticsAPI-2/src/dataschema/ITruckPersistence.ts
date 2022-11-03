037bbb0bab191b19067f8e757a7b433e12139427
prmika <miika.kauppinen@tuni.fi>
2022-11-03 09:20:37 +0000

    removed_some_folders

--- LogisticsAPI-2/src/dataschema/ITruckPersistence.ts
+++
@@ -1,8 +0,0 @@
-export interface ITruckPersistence {
-    domainId: string;
-    tare: Number;
-    load_capacity: Number;
-    max_battery_charge: Number;
-    autonomy: Number;
-    fast_charging_time: Number;
-  }
--- LogisticsAPI-2/src/services/IServices/IDeliveryPathService.ts
+++
@@ -1,9 +0,0 @@
-import { Result } from "../../core/logic/Result";
-import { IDeliveryPathDTO } from "../../dto/IDeliveryPathDTO";
-
-export default interface IDeliveryPathService  {
-    createDeliveryPath(deliverypathDTO: IDeliveryPathDTO): Promise<Result<IDeliveryPathDTO>>;
-    updateDeliveryPath(deliverypathId: string, deliverypathDTO: IDeliveryPathDTO): Promise<Result<IDeliveryPathDTO>>;
-    getDeliveryPath (deliverypathId: string): Promise<Result<IDeliveryPathDTO>>;
-    getAllDeliveryPaths(): Promise<Result<IDeliveryPathDTO[]>>;
-}
