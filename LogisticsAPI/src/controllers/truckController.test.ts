d777c8c911755a72d8272453dc6720f906882e6b
Jamerze <70536433+Jamerze@users.noreply.github.com>
2022-11-04 00:27:53 +0000

    ADD_DOCUMENTATION
    Added the Documentation for ARQSI

--- LogisticsAPI/src/controllers/truckController.test.ts
+++ LogisticsAPI/src/controllers/truckController.test.ts
@@ -8,6 +8,8 @@ import ITruckService from "../services/IServices/ITruckService";
 import TruckController from "../controllers/truckController";
 import { ITruckDTO } from '../dto/ITruckDTO';
 import { Truck } from '../domain/truck';
+import assert from 'assert';
+import { TruckMap } from '../mappers/TruckMap';
 
 describe('truck controller', function () {
 	const sandbox = sinon.createSandbox();
@@ -71,11 +73,12 @@ describe('truck controller', function () {
 			"fast_charging_time": req.body.fast_charging_time
 		}));
 	});
+	
 
 	it('truckController getTruck unit test using truckService stub', async function () {
 		// Arrange
 		let req: Partial<Request> = {};
-		req.params = {"truckId": "123"};
+		req.params = { "truckId": "123" };
 		let res: Partial<Response> = {
 			json: sinon.spy()
 		};
@@ -107,10 +110,61 @@ describe('truck controller', function () {
 		}));
 	});
 
+	it('truckController getAllTrucks unit test using truckService stub', async function () {
+		// Arrange
+		let req: Partial<Request> = {};
+		let res: Partial<Response> = {
+			json: sinon.spy()
+		};
+		let next: Partial<NextFunction> = () => { };
+
+		let truckServiceInstance = Container.get("TruckService");
+		sinon.stub(truckServiceInstance, "getAllTrucks").returns(Result.ok<ITruckDTO[]>([{
+			"id": "W01",
+			"tare": 29.70,
+			"load_capacity": 33.75,
+			"max_battery_charge": 43.7,
+			"autonomy": 32.74,
+			"fast_charging_time": 54.8
+		},
+		{
+			"id": "W02",
+			"tare": 30,
+			"load_capacity": 41.76,
+			"max_battery_charge": 43.7,
+			"autonomy": 24.87,
+			"fast_charging_time": 57.43
+		}]));
+
+		const ctrl = new TruckController(truckServiceInstance as ITruckService);
+
+		// Act
+		await ctrl.getAllTrucks(<Request>req, <Response>res, <NextFunction>next);
+
+		// Assert
+		sinon.assert.calledOnce(res.json);
+		sinon.assert.calledWith(res.json, sinon.match([{
+			"id": "W01",
+			"tare": 29.70,
+			"load_capacity": 33.75,
+			"max_battery_charge": 43.7,
+			"autonomy": 32.74,
+			"fast_charging_time": 54.8
+		},
+		{
+			"id": "W02",
+			"tare": 30,
+			"load_capacity": 41.76,
+			"max_battery_charge": 43.7,
+			"autonomy": 24.87,
+			"fast_charging_time": 57.43
+		}]));
+	});
+
 	it('truckController updateTruck unit test using truckService stub', async function () {
 		// Arrange
 		let body = {
-			"id":"W01",
+			"id": "W01",
 			"tare": 29.70,
 			"load_capacity": 33.75,
 			"max_battery_charge": 43.7,
@@ -119,14 +173,14 @@ describe('truck controller', function () {
 		};
 		let req: Partial<Request> = {};
 		req.body = body;
-		req.params = {"truckId": "W01"};
+		req.params = { "truckId": "W01" };
 		let res: Partial<Response> = {
 			json: sinon.spy()
 		};
 		let next: Partial<NextFunction> = () => { };
 
 		let truckServiceInstance = Container.get("TruckService");
-		sinon.stub(truckServiceInstance, "updateTruck").returns(Result.ok<ITruckDTO>({
+		let tempSpy = sinon.stub(truckServiceInstance, "updateTruck").returns(Result.ok<ITruckDTO>({
 			"id": req.body.id,
 			"tare": req.body.tare,
 			"load_capacity": req.body.load_capacity,
@@ -137,20 +191,129 @@ describe('truck controller', function () {
 
 		const ctrl = new TruckController(truckServiceInstance as ITruckService);
 
-		// Act
 		await ctrl.updateTruck(<Request>req, <Response>res, <NextFunction>next);
-
 		// Assert
-		sinon.assert.calledOnce(res.json);
-		sinon.assert.calledWith(res.json, sinon.match({
-			"id": req.body.id, "tare": req.body.tare,
-			"load_capacity": req.body.load_capacity,
-			"max_battery_charge": req.body.max_battery_charge,
-			"autonomy": req.body.autonomy,
-			"fast_charging_time": req.body.fast_charging_time
-		}));
+		tempSpy.callsFake(() => {
+			sinon.assert.calledOnce(res.json);
+			sinon.assert.calledWith(res.json, sinon.match({
+				"id": req.body.id, "tare": req.body.tare,
+				"load_capacity": req.body.load_capacity,
+				"max_battery_charge": req.body.max_battery_charge,
+				"autonomy": req.body.autonomy,
+				"fast_charging_time": req.body.fast_charging_time
+			}));
+			sinon.done();
+		});
+
 	});
 
+	it('Create truck', function () {
+		let truck: Result<Truck> = Truck.create({
+			"id": "123", "tare": 29.70,
+			"load_capacity": 33.75,
+			"max_battery_charge": 43.7,
+			"autonomy": 32.74,
+			"fast_charging_time": 54.8
+		} as ITruckDTO);
+
+		let convertedTruck: Truck = truck.getValue() as Truck;
+		assert.equal(convertedTruck.tare,29.70);  
+		assert.equal(convertedTruck.load_capacity,33.75);  
+		assert.equal(convertedTruck.max_battery_charge,43.7);  
+		assert.equal(convertedTruck.autonomy,32.74);  
+		assert.equal(convertedTruck.fast_charging_time,54.8);  
+
+		
+
+	});
+
+	it('Create and update truck', function () {
+		let truck: Result<Truck> = Truck.create({
+			"id": "123", "tare": 29.70,
+			"load_capacity": 33.75,
+			"max_battery_charge": 43.7,
+			"autonomy": 32.74,
+			"fast_charging_time": 54.8
+		} as ITruckDTO);
+
+		let convertedTruck: Truck = truck.getValue() as Truck;
+		assert.equal(convertedTruck.tare,29.70);  
+		assert.equal(convertedTruck.load_capacity,33.75);  
+		assert.equal(convertedTruck.max_battery_charge,43.7);  
+		assert.equal(convertedTruck.autonomy,32.74);  
+		assert.equal(convertedTruck.fast_charging_time,54.8);  
+
+		convertedTruck.tare = 30;
+		convertedTruck.load_capacity = 45;
+		convertedTruck.max_battery_charge = 90;
+		convertedTruck.autonomy = 25;
+		convertedTruck.fast_charging_time = 46.7;
+
+		//Values are replaced
+		assert.notEqual(convertedTruck.tare,29.70);  
+		assert.notEqual(convertedTruck.load_capacity,33.75);  
+		assert.notEqual(convertedTruck.max_battery_charge,43.7);  
+		assert.notEqual(convertedTruck.autonomy,32.74);  
+		assert.notEqual(convertedTruck.fast_charging_time,54.8);  
+
+		assert.equal(convertedTruck.tare,30);  
+		assert.equal(convertedTruck.load_capacity,45);  
+		assert.equal(convertedTruck.max_battery_charge,90);  
+		assert.equal(convertedTruck.autonomy,25);  
+		assert.equal(convertedTruck.fast_charging_time,46.7);  
+
+	});
+
+	it('Map truck data', function () {
+		let truck: Result<Truck> = Truck.create({
+			"id": "123", "tare": 29.70,
+			"load_capacity": 33.75,
+			"max_battery_charge": 43.7,
+			"autonomy": 32.74,
+			"fast_charging_time": 54.8
+		} as ITruckDTO);
+
+		
+
+		let convertedTruck: Truck = truck.getValue() as Truck;
+		let truckDto = TruckMap.toDTO(convertedTruck);
+		assert.equal(truckDto.tare,29.70);  
+		assert.equal(truckDto.load_capacity,33.75);  
+		assert.equal(truckDto.max_battery_charge,43.7);  
+		assert.equal(truckDto.autonomy,32.74);  
+		assert.equal(truckDto.fast_charging_time,54.8);  
+
+		let truckDomain = TruckMap.toDomain(convertedTruck);
+		assert.equal(truckDomain.tare,29.70);  
+		assert.equal(truckDomain.load_capacity,33.75);  
+		assert.equal(truckDomain.max_battery_charge,43.7);  
+		assert.equal(truckDomain.autonomy,32.74);  
+		assert.equal(truckDomain.fast_charging_time,54.8);  
+
+		let truckPersistence = TruckMap.toPersistence(convertedTruck);
+		assert.equal(truckPersistence.tare,29.70);  
+		assert.equal(truckPersistence.load_capacity,33.75);  
+		assert.equal(truckPersistence.max_battery_charge,43.7);  
+		assert.equal(truckPersistence.autonomy,32.74);  
+		assert.equal(truckPersistence.fast_charging_time,54.8);  
+	});
+
+	it('Check if 2 trucks are equal to each other', function () {
+		let truck: Result<Truck> = Truck.create({
+			"id": "123", "tare": 29.70,
+			"load_capacity": 33.75,
+			"max_battery_charge": 43.7,
+			"autonomy": 32.74,
+			"fast_charging_time": 54.8
+		} as ITruckDTO);
+
+		
+
+		let convertedTruck: Truck = truck.getValue() as Truck;
+		let convertedTruck2: Truck = truck.getValue() as Truck;
+		
+		assert.ok(convertedTruck.equals(convertedTruck2))
+	});
 
 	it('truckController + truckService integration test using truckRepoistory and Truck stubs', async function () {
 		// Arrange	
