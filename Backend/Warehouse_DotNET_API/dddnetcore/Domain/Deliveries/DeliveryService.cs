7d3e861836d0d1d278699f6064a738e55e7656e3
prmika <miika.kauppinen@tuni.fi>
2022-11-24 09:50:11 +0000

    ADDED_COMMENTS_TO_DELIVERIES

--- Backend/Warehouse_DotNET_API/dddnetcore/Domain/Deliveries/DeliveryService.cs
+++ Backend/Warehouse_DotNET_API/dddnetcore/Domain/Deliveries/DeliveryService.cs
@@ -18,21 +18,27 @@ namespace DDDNetCore.Domain.Deliveries
         {
             this._unitOfWork = unitOfWork;
         }
+
+        //gets all deliveries from database and returns list of deliveries
         public List<Delivery> GetAllDeliveries()
         {
             return _unitOfWork.DeliveryRepository.GetAll().ToList();
         }
 
+        //gets delivery which match with id and returns it 
         public Delivery GetOneDelivery(string id)
         {
             return _unitOfWork.DeliveryRepository.GetById(id);
         }
 
+        //adds delivery to the database
         public Delivery AddDelivery(DeliveryDto delivery)
         {
+            //gets warehouse with passed id from the database
             var war = _unitOfWork.WarehouseRepository.GetById(delivery.WarehouseId);
+            //checks if warehouse exist -- if not returns error message 
             if (war == null){
-                throw new BusinessRuleValidationException($"Warehous id:{delivery.WarehouseId} does not exist");
+                throw new BusinessRuleValidationException($"Warehouse id:{delivery.WarehouseId} does not exist");
             }
             else {
             Delivery deliveryToCreate = new Delivery(delivery.Id, delivery.DeliveryDate, delivery.Weight, delivery.WarehouseId, delivery.TimeToPickup, delivery.TimeToPlace);
@@ -42,13 +48,17 @@ namespace DDDNetCore.Domain.Deliveries
             }
         }
 
+        //updates delivery from database
         public Delivery UpdateDelivery(DeliveryDto delivery)
         {
+            //gets warehouse with passed id from the database
             var war = _unitOfWork.WarehouseRepository.GetById(delivery.WarehouseId);
+            //checks if warehouse exist
             if (war == null){
                 throw new BusinessRuleValidationException($"Warehous id:{delivery.WarehouseId} does not exist");
             }
             else{
+            //updates the delivery
             Delivery deliveryToUpdate = new Delivery(delivery.Id,  delivery.DeliveryDate, delivery.Weight, delivery.WarehouseId, delivery.TimeToPickup, delivery.TimeToPlace);
             _unitOfWork.DeliveryRepository.Update(deliveryToUpdate);
             _unitOfWork.Commit();
