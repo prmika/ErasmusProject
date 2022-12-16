8a649baab5b608229f396cc0cb158d1864f262d8
Ben1782 <65095744+Ben1782@users.noreply.github.com>
2022-12-16 16:05:01 +0000

    Move Road files to folders
    3.3.1.3.a - Add truck model to scene
    *****
    Clean code and move files into their respective folder (Textures, Models, etc...)

--- Frontend/lab5frontend/src/Road/Road.js
+++ Frontend/lab5frontend/src/Road/Road.js
@@ -1,4 +1,7 @@
-class Road {
+import * as THREE from "three";
+import {DoubleSide} from "three";
+
+export class Road {
     //Constructeur de la classe Road
     constructor(width, city1, city2){
     this.width = width;
@@ -6,7 +9,64 @@ class Road {
     this.city2 = city2;
     }
 
-    addToScene(){
-        //TBC
+    addToScene(circleRadius, positions, scene){
+        let roadGeometry = new THREE.BufferGeometry();
+        //We load the texture of the road
+        let loader = new THREE.TextureLoader();
+
+
+        const roadWidth = 2;
+        //connectorScale1/2 are used to harmonize the size of all city connectors. The size will be 1.5 times the circle radius
+        const connectorScale1 = 1.5 * circleRadius/Math.abs((positions[this.city2][0] - positions[this.city1][0]));
+        const connectorScale2 = 1.5 * circleRadius/Math.abs((positions[this.city1][0] - positions[this.city2][0]));
+        //These two variables are used to orient the city connectors to the next warehouse.
+        const orientXCity1ToRoad = (positions[this.city2][0] - positions[this.city1][0]) * connectorScale1;
+        const orientXRoadToCity2 = (positions[this.city1][0] - positions[this.city2][0]) * connectorScale2;
+
+        //This array contains all the points needed to form the triangles that will form the road. 2 triangles will form a rectangle.
+        // The (+ roadWidth/2) and (- roadWidth/2) are used to center the road with the circle of the cities.
+        // The (+ (positions[city2][0] - positions[city1][0]) * 0.1) and (- (positions[city2][0] - positions[city1][0]) * 0.1) are used to orient the connector to the next city, and to set up the length of this connector.
+        //The (positions(city][2]-0.01) puts the road in the bottom (infinitesimal) of the city.
+        const vertices = new Float32Array( [
+            //First Connector
+            positions[this.city1][0], positions[this.city1][1] - roadWidth/2,  positions[this.city1][2] - 0.01, //centre bas OK
+            positions[this.city1][0] + orientXCity1ToRoad, positions[this.city1][1] - roadWidth/2,  positions[this.city1][2] - 0.01, //droite bas OK
+            positions[this.city1][0] + orientXCity1ToRoad, positions[this.city1][1] + roadWidth/2,  positions[this.city1][2] - 0.01, //droite haut OK
+
+
+            positions[this.city1][0] + orientXCity1ToRoad, positions[this.city1][1] + roadWidth/2,  positions[this.city1][2] - 0.01, //droite haut OK
+            positions[this.city1][0], positions[this.city1][1] + roadWidth/2,  positions[this.city1][2] - 0.01, //centre haut OK
+            positions[this.city1][0], positions[this.city1][1] - roadWidth/2,  positions[this.city1][2] - 0.01, //centre bas OK
+
+            //The ramp
+            positions[this.city1][0] + orientXCity1ToRoad, positions[this.city1][1] - roadWidth/2,  positions[this.city1][2] - 0.01, //connector1 (gauche) bas OK
+            positions[this.city2][0] + orientXRoadToCity2,  positions[this.city2][1] - roadWidth/2,  positions[this.city2][2] - 0.01, //connector2(droite) bas
+            positions[this.city2][0] + orientXRoadToCity2,  positions[this.city2][1] + roadWidth/2,  positions[this.city2][2] - 0.01, //connector2(droite) haut
+
+            positions[this.city2][0] + orientXRoadToCity2,  positions[this.city2][1] + roadWidth/2,  positions[this.city2][2] - 0.01, //connector2(droite) haut
+            positions[this.city1][0] + orientXCity1ToRoad, positions[this.city1][1] + roadWidth/2,  positions[this.city1][2] - 0.01, //connector1(gauche) haut OK
+            positions[this.city1][0] + orientXCity1ToRoad, positions[this.city1][1] - roadWidth/2,  positions[this.city1][2] - 0.01, //connector1(gauche) bas OK
+
+            //Second Connector
+            positions[this.city2][0] + orientXRoadToCity2, positions[this.city2][1] - roadWidth/2,  positions[this.city2][2] - 0.01, //gauche bas OK
+            positions[this.city2][0], positions[this.city2][1] - roadWidth/2,  positions[this.city2][2] - 0.01, //centre bas OK
+            positions[this.city2][0], positions[this.city2][1] + roadWidth/2,  positions[this.city2][2] - 0.01, //centre haut OK
+
+
+            positions[this.city2][0], positions[this.city2][1] + roadWidth/2,  positions[this.city2][2] - 0.01, //centre haut OK
+            positions[this.city2][0] + orientXRoadToCity2, positions[this.city2][1] + roadWidth/2,  positions[this.city2][2] - 0.01, //gauche haut OK
+            positions[this.city2][0] + orientXRoadToCity2, positions[this.city2][1] - roadWidth/2,  positions[this.city2][2] - 0.01 //gauche bas OK
+
+        ] );
+        let texture_road = loader.load('./textures/texture_road.jpg');
+        texture_road.wrapS = THREE.RepeatWrapping;
+        texture_road.wrapT = THREE.RepeatWrapping;
+        roadGeometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+        let texture_sky = loader.load('./textures/texture_sky.jpg');
+        scene.background = texture_sky;
+        //We set the roads in asphalt grey and we enable the DoubleSide attribute which allows us to see the road from the two sides.
+        const roadMaterial = new THREE.MeshBasicMaterial( { map: texture_road, side: DoubleSide } );
+        const roadMesh = new THREE.Mesh( roadGeometry, roadMaterial );
+        scene.add(roadMesh);
     }
 }
--- Frontend/lab5frontend/src/Road/index.js
+++ Frontend/lab5frontend/src/Road/index.js
@@ -5,6 +5,7 @@ import {CircleGeometry, Object3D} from "three";
 import { FBXLoader } from 'https://cdn.skypack.dev/three@0.138.0/examples/jsm/loaders/FBXLoader';
 import {DoubleSide} from "three";
 import { GLTFLoader } from "./GLTFLoader.js";
+import { Road } from "./Road.js";
 
 const gui = new dat.GUI()
 
@@ -109,26 +110,10 @@ const visibleWidthAtZDepth = ( depth, camera ) => {
     return height * camera.aspect;
 };
 
-
-
-//////////////////////////////////////////////
-/////////////CREATION OF PLANE///////////////
-
-//This plane is not useful. We just use it to debug the program.
-const planeGeometry = new THREE.PlaneGeometry(2,2,10,10);
-console.log(planeGeometry);
-const planeMaterial = new THREE.MeshPhongMaterial({color: 0xFF0000, side: THREE.DoubleSide}); //MeshPhongMaterial : Material that reacts to the light
-planeMaterial.flatShading = true;
-const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-//planeMesh.rotateZ(Math.pow(Math.atan((positions[1][1]-positions[0][1])/(positions[1][0]-positions[0][0])),2));
-//scene.add(planeMesh);
-
 ////////////////////////////////////////////////////////////////////////////////
-/////////////////DEFINITION OF CITIES AND WAREHOUSES////////////////////////////
+/////////////////DEFINITION OF WAREHOUSES LOCATIONS////////////////////////////
 
-//Warehouses :
-
-//longitude, latitude, altitude OR in three js axis : z, x, y
+//Warehouses longitude, latitude, altitude OR in three js axis : z, x, y
 const positions = [
     [8.2451,40.9321,250],
     [8.6410,41.0072,550],
@@ -149,9 +134,8 @@ const positions = [
     [8.6118, 41.1239, 500]
 ];
 
-//We define the radius of the circles that will represents the cities.
+//We define the radius of the circles that will represent the cities.
 const circleRadius = 3;
-// const circleRadius = visibleHeightAtZDepth(camera.position.z - 5,camera) / (positions.length * 2);
 const circleGeometry = new THREE.CircleGeometry(circleRadius,50,0);
 const circleMaterial = new THREE.MeshPhongMaterial({color: 0x00FF00, side: THREE.DoubleSide});
 
@@ -232,7 +216,7 @@ controls.update(); //Must be called after any manual changes to the camera's tra
 ////////////IMPORT OF TRUCK MODEL/////////////
 const loader = new GLTFLoader();
 let truck;
-new GLTFLoader().load('truck2.glb', (truckModel) => {
+new GLTFLoader().load("./models/gltf/truck.glb", (truckModel) => {
     truck = truckModel.scene;
     if(positions[0]) truck.position.set(positions[0][0],positions[0][1],positions[0][2]);
     truck.rotateX(Math.PI/2);
@@ -251,7 +235,7 @@ let warehouses = []; //This array will contain all the warehouses models.
 
 //We add a warehouse 3d model in the scene at the side of each city.
 for (let i = 0; i<positions.length;i++) {
-    loader.load('warehouse_model.fbx', function (warehouseModel) {
+    loader.load('./models/fbx/warehouse_model.fbx', function (warehouseModel) {
         warehouseModel.traverse(function (child) {
             if (child.isMesh) {
                 child.castShadow = true;
@@ -274,88 +258,23 @@ function getRandomInt(max) {
 }
 
 //This function add a road between the city 1 and the city 2
-function addRoadBetweenCities(city1,city2){
-    //////////////////////////////////////////////////////
-/////////////CREATION OF ORIENTED PLANE///////////////
-    let roadGeometry = new THREE.BufferGeometry();
-    //We load the texture of the road
-    let loader = new THREE.TextureLoader();
-
-
-    //let connectorLength = 6;
-    const roadWidth = 2;
-    //connectorScale1/2 are used to harmonize the size of all city connectors. The size will be 1.5 times the circle radius
-    const connectorScale1 = 1.5 * circleRadius/Math.abs((positions[city2][0] - positions[city1][0]));
-    const connectorScale2 = 1.5 * circleRadius/Math.abs((positions[city1][0] - positions[city2][0]));
-    //These two variables are used to orient the city connectors to the next warehouse.
-    const orientXCity1ToRoad = (positions[city2][0] - positions[city1][0]) * connectorScale1;
-    const orientXRoadToCity2 = (positions[city1][0] - positions[city2][0]) * connectorScale2;
-    /*
-    const orientYCity1ToRoad = (positions[city2][1] - positions[city1][1]) * connectorScale1;
-    const orientYRoadToCity2 = (positions[city1][1] - positions[city2][1]) * connectorScale1;
-    */
-
-    //This array contains all the points needed to form the triangles that will form the road. 2 triangles will form a rectangle.
-    // The (+ roadWidth/2) and (- roadWidth/2) are used to center the road with the circle of the cities.
-    // The (+ (positions[city2][0] - positions[city1][0]) * 0.1) and (- (positions[city2][0] - positions[city1][0]) * 0.1) are used to orient the connector to the next city, and to set up the length of this connector.
-    //The (positions(city][2]-0.01) puts the road in the bottom (infinitesimal) of the city.
-    const vertices = new Float32Array( [
-        //First Connector
-        positions[city1][0], positions[city1][1] - roadWidth/2,  positions[city1][2] - 0.01, //centre bas OK
-        positions[city1][0] + orientXCity1ToRoad, positions[city1][1] - roadWidth/2,  positions[city1][2] - 0.01, //droite bas OK
-        positions[city1][0] + orientXCity1ToRoad, positions[city1][1] + roadWidth/2,  positions[city1][2] - 0.01, //droite haut OK
-
-
-        positions[city1][0] + orientXCity1ToRoad, positions[city1][1] + roadWidth/2,  positions[city1][2] - 0.01, //droite haut OK
-        positions[city1][0], positions[city1][1] + roadWidth/2,  positions[city1][2] - 0.01, //centre haut OK
-        positions[city1][0], positions[city1][1] - roadWidth/2,  positions[city1][2] - 0.01, //centre bas OK
-
-        //The ramp
-        positions[city1][0] + orientXCity1ToRoad, positions[city1][1] - roadWidth/2,  positions[city1][2] - 0.01, //connector1 (gauche) bas OK
-        positions[city2][0] + orientXRoadToCity2,  positions[city2][1] - roadWidth/2,  positions[city2][2] - 0.01, //connector2(droite) bas
-        positions[city2][0] + orientXRoadToCity2,  positions[city2][1] + roadWidth/2,  positions[city2][2] - 0.01, //connector2(droite) haut
-
-        positions[city2][0] + orientXRoadToCity2,  positions[city2][1] + roadWidth/2,  positions[city2][2] - 0.01, //connector2(droite) haut
-        positions[city1][0] + orientXCity1ToRoad, positions[city1][1] + roadWidth/2,  positions[city1][2] - 0.01, //connector1(gauche) haut OK
-        positions[city1][0] + orientXCity1ToRoad, positions[city1][1] - roadWidth/2,  positions[city1][2] - 0.01, //connector1(gauche) bas OK
-
-        //Second Connector
-        positions[city2][0] + orientXRoadToCity2, positions[city2][1] - roadWidth/2,  positions[city2][2] - 0.01, //gauche bas OK
-        positions[city2][0], positions[city2][1] - roadWidth/2,  positions[city2][2] - 0.01, //centre bas OK
-        positions[city2][0], positions[city2][1] + roadWidth/2,  positions[city2][2] - 0.01, //centre haut OK
-
-
-        positions[city2][0], positions[city2][1] + roadWidth/2,  positions[city2][2] - 0.01, //centre haut OK
-        positions[city2][0] + orientXRoadToCity2, positions[city2][1] + roadWidth/2,  positions[city2][2] - 0.01, //gauche haut OK
-        positions[city2][0] + orientXRoadToCity2, positions[city2][1] - roadWidth/2,  positions[city2][2] - 0.01 //gauche bas OK
-    ] );
-
-    let texture_road = loader.load('texture_road.jpg');
-    texture_road.wrapS = THREE.RepeatWrapping;
-    texture_road.wrapT = THREE.RepeatWrapping;
-    roadGeometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-    let texture_sky = loader.load('texture_sky.jpg');
-    scene.background = texture_sky;
-    //We set the roads in asphalt grey and we enable the DoubleSide attribute which allows us to see the road from the two sides.
-    const roadMaterial = new THREE.MeshBasicMaterial( { map: texture_road, side: DoubleSide } );
-    const roadMesh = new THREE.Mesh( roadGeometry, roadMaterial );
-    scene.add(roadMesh);
-}
+let route = new Road(1,2,3);
+console.log(route);
 
 //We connect each city to 2 other random cities.
 for (let i=0; i<positions.length; i++){
     let randomCity = 0; //We generate the random id of the city.
     do{
-        randomCity = getRandomInt(positions.length);
-    }while(randomCity == i); //This makes impossible to connect a city to itself.
-    addRoadBetweenCities(i,randomCity); //We connect the 2 cities.
-    /*
-    let randomCity2
+        route.city2 = getRandomInt(positions.length);
+    }while(route.city2 == i); //This makes impossible to connect a city to itself
+    route.city1 = i;
+    route.addToScene(circleRadius, positions, scene);// We connect the 2 cities.
+/*
     do{
-       randomCity2 = getRandomInt(positions.length);
-       addRoadBetweenCities(i, randomCity2); //We connect the 2 cities.
-    }while (randomCity2 == randomCity || randomCity2 == i); //This makes impossible to connect a city to itself or to the precedent city.
-    */
+       route.city2 = getRandomInt(positions.length);
+        route.addToScene(circleRadius, positions, scene);// We connect the 2 cities.
+    }while (route.city2 == randomCity || route.city2 == i); //This makes impossible to connect a city to itself or to the precedent city.
+*/
 }
 
 controls.target.set( (xMax+xMin)/2, (yMax+yMin)/2, positions[2][2] ); //We put the center of the orbit controller at the middle of the cities to  make the control more convenient.
