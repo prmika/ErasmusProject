00b8c2ddfa5a08dbadec823cc316564d815bc463
PPatricc <paspatryk12@gmail.com>
2023-01-08 17:03:05 +0000

    UPDATED_UNIT_TESTING

--- Frontend/lab5frontend/src/app/components/useradd/useradd.component.spec.ts
+++ Frontend/lab5frontend/src/app/components/useradd/useradd.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UseraddComponent } from './useradd.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { AuthService } from '@auth0/auth0-angular';
 
 describe('UseraddComponent', () => {
   let component: UseraddComponent;
   let fixture: ComponentFixture<UseraddComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['loginWithRedirect']);
+
     await TestBed.configureTestingModule({
-      declarations: [ UseraddComponent ]
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: AuthService, useValue: authServiceSpy }],
+      declarations: [UseraddComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(UseraddComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Component created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('There is a possibility to add a user', () => {
+    expect(component.addUser).toBeTruthy();
   });
 });
